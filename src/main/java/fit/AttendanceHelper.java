1f77f23befd008b6094eed3b1de53b85a4cf35e3
Neilton Andrade <neilton.saints@live.com>
2021-02-08 18:14:46 -0300

    alteração test

---
+++ src/main/java/fit/AttendanceHelper.java
@@ -0,0 +1,24 @@
+package fit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AttendanceHelper {
+
+public static Map<String, Boolean> random (int amount, int percentage){
+    int result = (amount * percentage) / 100;
+
+    Map<String, Boolean> listUser = new HashMap<>();
+
+    for (int i = 0; i < result; i++) {
+        listUser.put("user" + i, true);
+    }
+
+    for (int i = 0; i < amount - result; i++) {
+        listUser.put("userTotal" + i, false);
+    }
+
+
+    return listUser;
+}
+}
